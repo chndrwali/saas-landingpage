66842f512ad1b428fc47e56034ff0aad664cfb1e
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-11-30 19:24:55 +0700

    add: end

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import Download from '@/components/sections/download';
+import { Faq } from '@/components/sections/faq';
 import { Features } from '@/components/sections/features';
+import { Footer } from '@/components/sections/footer';
 import { Header } from '@/components/sections/header';
 import { Hero } from '@/components/sections/hero';
+import { Pricing } from '@/components/sections/pricing';
+import { Testimonials } from '@/components/sections/testimonials';
 
 export default function Home() {
   return (
@@ -8,6 +13,11 @@ export default function Home() {
       <Header />
       <Hero />
       <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
     </main>
   );
 }
